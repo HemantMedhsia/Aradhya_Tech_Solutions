50729c77bbe737829e92377987894df832639fb8
Hemant Medhsia <142498656+MyCoddingJourney@users.noreply.github.com>
2024-07-30 12:10:58 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+        <div className="bg-slate-700 w-full h-[100vh]">
+          Hello Aradhya Tech
+        </div>
+    </>
+  )
+}
+
+export default App
